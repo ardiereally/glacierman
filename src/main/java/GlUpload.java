b0f93c19eea1d5d80281a5b8c0007b67c817639b
rdas <5216575+rdas1212@users.noreply.github.com>
2020-07-06 13:46:03 +0530

    Adding GlUpload implementation

--- src/main/java/GlUpload.java
+++ src/main/java/GlUpload.java
@@ -1,2 +1,65 @@
+import com.amazonaws.services.glacier.AmazonGlacier;
+import com.amazonaws.services.glacier.AmazonGlacierClientBuilder;
+import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
+import com.amazonaws.services.glacier.transfer.ArchiveTransferManagerBuilder;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Uploader of archives to AWS Glacier
+ */
 public class GlUpload {
+    private final String archivePath;
+    private final String glacierVault;
+    private final ArchiveTransferManager archiveTransferManager;
+
+    public GlUpload(String archivePath, String glacierVault) {
+        this.archivePath = archivePath;
+        this.glacierVault = glacierVault;
+
+        AmazonGlacier client = AmazonGlacierClientBuilder
+                .standard()
+                .build();
+        this.archiveTransferManager = new ArchiveTransferManagerBuilder()
+                .withGlacierClient(client)
+                .build();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Starting glupload...");
+
+        String vaultName = Objects.requireNonNull(System.getProperty("vault"));
+        String archivePath = Objects.requireNonNull(System.getProperty("archive"));
+
+        if (List.of(vaultName, archivePath).contains("")) {
+            System.err.println("Required argument not supplied. Need all of: vault, archive");
+            System.exit(1);
+        }
+
+        System.out.println("Will upload " + archivePath + " to vault " + vaultName + " in " + System.getenv("AWS_REGION"));
+
+        GlUpload glUpload = new GlUpload(archivePath, vaultName);
+        System.out.println("Initialized.");
+
+        System.out.println("Starting upload....");
+        String archiveId = glUpload.upload();
+        System.out.println("Upload done. Archive ID is " + archiveId);
+        System.out.println("Done. Exiting...");
+    }
+
+    /**
+     * Upload the specified archive to the vault
+     *
+     * @return The archiveId
+     */
+    public String upload() throws FileNotFoundException {
+        return archiveTransferManager.upload(
+                glacierVault,
+                "Glacier backup of " + archivePath,
+                new File(archivePath)
+        ).getArchiveId();
+    }
 }
