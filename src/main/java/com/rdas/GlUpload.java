6bc40f9119d6fd4b1ad68194e5d79291baeba910
rdas <5216575+rdas1212@users.noreply.github.com>
2020-07-09 01:15:20 +0530

    Credential refactoring & stuff

--- src/main/java/com/rdas/GlUpload.java
+++ src/main/java/com/rdas/GlUpload.java
@@ -1,12 +1,21 @@
 package com.rdas;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.glacier.AmazonGlacier;
 import com.amazonaws.services.glacier.AmazonGlacierClientBuilder;
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManagerBuilder;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -18,15 +27,24 @@ public class GlUpload {
     private final String glacierVault;
     private final ArchiveTransferManager archiveTransferManager;
 
-    public GlUpload(String archivePath, String glacierVault) {
+    public GlUpload(String archivePath, String glacierVault, Credentials creds) {
         this.archivePath = archivePath;
         this.glacierVault = glacierVault;
 
         AmazonGlacier client = AmazonGlacierClientBuilder
                 .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(creds.getAccessKeyId(), creds.getSecretAccessKey())))
+                .withRegion(creds.getRegion())
                 .build();
+        AmazonSQS sqsClient = AmazonSQSClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(creds.getAccessKeyId(), creds.getSecretAccessKey()))).withRegion(creds.getRegion()).build();
+        AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(creds.getAccessKeyId(), creds.getSecretAccessKey()))).withRegion(creds.getRegion()).build();
         this.archiveTransferManager = new ArchiveTransferManagerBuilder()
                 .withGlacierClient(client)
+                .withSqsClient(sqsClient)
+                .withSnsClient(snsClient)
                 .build();
     }
 
@@ -41,9 +59,13 @@ public class GlUpload {
             System.exit(1);
         }
 
-        System.out.println("Will upload " + archivePath + " to vault " + vaultName + " in " + System.getenv("AWS_REGION"));
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader("credentials.json"));
+        Credentials creds = gson.fromJson(reader, Credentials.class);
 
-        GlUpload glUpload = new GlUpload(archivePath, vaultName);
+        System.out.println("Will upload " + archivePath + " to vault " + vaultName + " in " + creds.getRegion());
+
+        GlUpload glUpload = new GlUpload(archivePath, vaultName, creds);
         System.out.println("Initialized.");
 
         System.out.println("Starting upload....");
@@ -63,5 +85,38 @@ public class GlUpload {
                 "Glacier backup of " + archivePath,
                 new File(archivePath)
         ).getArchiveId();
+    }
+
+    public static class Credentials {
+        private String accessKeyId;
+        private String secretAccessKey;
+        private String region;
+
+        public Credentials() {
+        }
+
+        public String getAccessKeyId() {
+            return accessKeyId;
+        }
+
+        public void setAccessKeyId(String accessKeyId) {
+            this.accessKeyId = accessKeyId;
+        }
+
+        public String getSecretAccessKey() {
+            return secretAccessKey;
+        }
+
+        public void setSecretAccessKey(String secretAccessKey) {
+            this.secretAccessKey = secretAccessKey;
+        }
+
+        public String getRegion() {
+            return region;
+        }
+
+        public void setRegion(String region) {
+            this.region = region;
+        }
     }
 }
